1be2bf8f3c5a3fc28e40ff0744f93919aa3fb18b
JeremyDeAvilaInsoftar <jeremy.deavila@insoftar.com>
2022-11-06 20:53:04 -0500

    using rest java

--- src/main/java/com/integracion/IntegracionApplication.java
+++ src/main/java/com/integracion/IntegracionApplication.java
@@ -2,12 +2,18 @@ package com.integracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class IntegracionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntegracionApplication.class, args);
     }
 
+    @GetMapping(path = "/")
+    public String sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
+        return "Hello " + name;
+    }
 }
