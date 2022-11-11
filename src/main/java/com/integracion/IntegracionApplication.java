3c93fc7b7a1c4d572dc7fe3e50677b9912c7ab51
JeremyDeAvilaInsoftar <jeremy.deavila@insoftar.com>
2022-11-10 21:24:54 -0500

    mysql

--- src/main/java/com/integracion/IntegracionApplication.java
+++ src/main/java/com/integracion/IntegracionApplication.java
@@ -1,19 +1,30 @@
 package com.integracion;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class IntegracionApplication {
+
+    @Autowired
+    private UserRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(IntegracionApplication.class, args);
     }
 
     @GetMapping(path = "/")
-    public String sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
-        return "Hello " + name;
+    public String listAll(Model model) {
+        List<User> listUsers = repo.findAll();
+        model.addAttribute("listUsers", listUsers);
+        return "users";
     }
+
 }
---
+++ src/main/java/com/integracion/User.java
@@ -0,0 +1,16 @@
+package com.integracion;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+}
